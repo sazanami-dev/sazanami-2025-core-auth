73895cb728c05cbe866e2938e884d3f6b2bd4264
rca <nem@nem.x0.to>
2025-08-26 06:24:00 +0900

    feat: add swagger startup

---
+++ src/boot/swagger.ts
@@ -0,0 +1,36 @@
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import { Router } from 'express';
+import { EnvUtil, EnvKey } from '@/utils/env-util';
+import { registry } from '@/openapi-registry';
+import { OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi';
+import Logger from '@/logger';
+
+export const setupSwagger = (app: Router, logger: Logger) => {
+  if (EnvUtil.get(EnvKey.NODE_ENV) === 'production') {
+    return; // 本番環境ではSwaggerを有効にしない
+  }
+
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+  const openApiDocument = generator.generateDocument({
+    openapi: '3.0.0',
+    info: {
+      title: "Core-Auth API Documentation",
+      version: '1.0.0',
+      description: '文化祭2025基幹認証システム'
+    },
+    servers: [
+      {
+        url: `http://localhost:${EnvUtil.get(EnvKey.PORT)}`,
+        description: 'Local server',
+      },
+    ]
+  });
+
+  const swaggerRouter = Router();
+  swaggerRouter.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+
+  app.use('/api-docs', swaggerRouter);
+
+  logger.info(`Swagger UI is available at http://localhost:${EnvUtil.get(EnvKey.PORT)}/api-docs`, 'swagger');
+}
