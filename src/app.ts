4e493f8b158793bf24f631604a222078377d3704
rca <nem@nem.x0.to>
2025-09-24 02:51:38 +0900

    impl: import and execute setup middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { setupSwagger } from '@/boot/swagger';
 import Logger from '@/logger';
 import { testPsqlConnection } from './boot/test-connection';
+import { setupMiddlewares } from './boot/middlewares';
 
 export const createApp = async () => {
   const app = express();
   const logger = new Logger('boot');
 
   setupSwagger(app, logger);
+
+  setupMiddlewares(app);
 
   await testPsqlConnection();
 
