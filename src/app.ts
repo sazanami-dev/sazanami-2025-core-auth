d2d70138bc5a113d79693ba661ef45038b4a0b3f
rca <nem@nem.x0.to>
2025-09-24 02:11:22 +0900

    impl: import and execute db connection testing when startup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { setupSwagger } from '@/boot/swagger';
 import Logger from '@/logger';
+import { testPsqlConnection } from './boot/test-connection';
 
 export const createApp = async () => {
   const app = express();
   const logger = new Logger('boot');
 
   setupSwagger(app, logger);
+
+  await testPsqlConnection();
+
+  
 
   return app;
 }
