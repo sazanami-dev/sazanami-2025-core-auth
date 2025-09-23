c47ec7471c1421366db081affde1318f99476820
rca <nem@nem.x0.to>
2025-09-24 06:10:35 +0900

    wip:

--- src/routes/authenticate.ts
+++ src/routes/authenticate.ts
@@ -1,35 +1,36 @@
 import { Router } from "express";
 import { DoResponse } from "@/utils/do-resnpose";
-import { verifyRegCodeAndResolveUser } from "@/services/auth/regCode";
-import { EnvKey, EnvUtil } from "@/utils/env-util";
+import { createAnonymousSession, verifySessionIdAndResolveUser } from "@/services/auth/session";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   // Get query parameters
   const redirectUrl = req.query.redirectUrl as string | undefined;
+  const postbackUrl = req.query.postbackUrl as string | undefined;
   const state = req.query.state as string | undefined;
-  const regCode = req.query.regCode as string | undefined;
+  const cookies = req.cookies;
+  let sessionId: string | undefined;
 
-  if (!redirectUrl) return DoResponse.init(res).badRequest().errorMessage('redirectUrl query parameter is required').send();
-
-  if (regCode) {
-    // When initiating registration, redirect to registration page
-    const user = await verifyRegCodeAndResolveUser(regCode);
-    if (!user) {
-      return DoResponse.init(res).badRequest().errorMessage('Invalid registration code').send();
-    }
-
-    const initPage = EnvUtil.get(EnvKey.ACCOUNT_INITIALIZATION_PAGE);
-
-    const url = new URL(initPage);
-    if (state) url.searchParams.append('state', state);
-    url.searchParams.append('redirectUrl', redirectUrl);
-
-    return DoResponse.init(res).ok().json({ redirectTo: url.toString() }).send();
+  if (!(redirectUrl || postbackUrl)) {
+    return DoResponse.init(res).badRequest().errorMessage('Either redirectUrl or postbackUrl query parameter is required').send();
   }
 
-  // TODO: login
+  if (!cookies || !cookies.sessionId) {
+    createAnonymousSession().then(session => {
+      sessionId = session.id;
+    }).then(() => {
+      res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days
+      });
+    });
+
+    // redirect
+  }
+
+  // todo
 
 });
 
