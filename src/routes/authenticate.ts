fd76d3ab7c3ea3c242a787ddedd95e8d38d43658
rca <nem@nem.x0.to>
2025-10-05 23:27:31 +0900

    wip: add memos

--- src/routes/authenticate.ts
+++ src/routes/authenticate.ts
@@ -32,11 +32,16 @@ router.get('/', async (req, res) => {
     // Save pending redirect
     await createPendingRedirect(sessionId!, redirectUrl, postbackUrl, state);
 
+    // TODO: 初期設定用のトークンを発行して何らかの方法で付与する
+
     // Redirect to login page
     return DoResponse.init(res).redirect(EnvUtil.get(EnvKey.ACCOUNT_INITIALIZATION_PAGE)).send();
   }
 
-  // todo
+  // TODO: 実装する
+  // セッションIDがある場合はユーザー情報を取得して認証状態を確認
+  // →認証されていればそのままリダイレクト
+  // →認証されていなければQRコードを再度読み取って認証するよう促す
 
 });
 
