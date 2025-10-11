7ab4f6c4f7c390511c7b115a458fc90fe767af79
rca <nem@nem.x0.to>
2025-10-11 23:24:31 +0900

    impl: initialize route wip

--- src/routes/initialize.ts
+++ src/routes/initialize.ts
@@ -1,10 +1,40 @@
 import { Router } from "express";
 import { DoResponse } from "@/utils/do-resnpose";
 import { createAnonymousSession, verifySessionIdAndResolveUser } from "@/services/auth/session";
+import { issueToken, makeClaimsHelper } from "@/services/auth/token";
+import { EnvKey, EnvUtil } from "@/utils/env-util";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   // regCodeを受け取ってユーザー初期化処理を開始する
+
+  const reqCode = req.query.regCode as string | undefined;
+  const cookies = req.cookies;
+  let sessionId: string | undefined;
+
+  if (!reqCode) {
+    return DoResponse.init(res).badRequest().errorMessage('regCode query parameter is required').send();
+  }
+
+  if (!cookies || !cookies.sessionId) {
+    await createAnonymousSession().then(session => {
+      sessionId = session.id;
+    }).then(() => {
+      res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days
+      });
+    });
+  } else sessionId = cookies.sessionId as string;
+
+  // 初期設定用トークンを発行し、リダイレクトする
+  const token = await issueToken(makeClaimsHelper(sessionId!));
+
+  const url = new URL(EnvUtil.get(EnvKey.ACCOUNT_INITIALIZATION_PAGE));
+  url.searchParams.append('token', token);
+
+  return DoResponse.init(res).redirect(url.toString()).send();
 });
 
