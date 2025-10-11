5161cb9ef3264f49371f2a29458d5f88fda7ed89
rca <nem@nem.x0.to>
2025-10-11 23:24:39 +0900

    impl: get user info route wip

--- src/routes/i.ts
+++ src/routes/i.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { DoResponse } from "@/utils/do-resnpose";
+import { getUserWithSessionBySessionId } from "@/services/auth/user";
+import { UserWithSessionSchema } from "@/schemas/object/User";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  // ユーザー情報を返す
+  const cookies = req.cookies;
+  const sessionId = cookies ? (cookies.sessionId as string | undefined) : undefined;
+
+  if (!sessionId) {
+    return DoResponse.init(res).unauthorized().errorMessage('No session').send();
+  }
+
+  const userWithSession = await getUserWithSessionBySessionId(sessionId).catch(() => null);
+
+  if (!userWithSession) {
+    return DoResponse.init(res).unauthorized().errorMessage('Invalid session').send();
+  }
+
+  return DoResponse.init(res).ok().validatedJson(userWithSession, UserWithSessionSchema).send();
 });
 
 router.put('/', async (req, res) => {
