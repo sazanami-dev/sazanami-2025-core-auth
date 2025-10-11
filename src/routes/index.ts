4d8957cb15a548fbeb05d61db7cbf410daa5de1e
rca <nem@nem.x0.to>
2025-10-11 23:26:45 +0900

    impl: register routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import iRoute from "./i";
+import authenticateRoute from "./authenticate";
+import initializeRoute from "./initialize";
 
 const router = Router();
+
+router.use('/i', iRoute);
+router.use('/authenticate', authenticateRoute);
+router.use('/initialize', initializeRoute);
 
 export default router;
