9148773149a3e30c41b4e8e3043442e87d811762
rca <nem@nem.x0.to>
2025-10-11 16:55:59 +0900

    impl: add skeleton

---
+++ src/routes/i.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  // ユーザー情報を返す
+});
+
+router.put('/', async (req, res) => {
+  // ユーザー情報を書き換える
+});
--- src/routes/initialize.ts
+++ src/routes/initialize.ts
@@ -1,1 +1,10 @@
-// todo
+import { Router } from "express";
+import { DoResponse } from "@/utils/do-resnpose";
+import { createAnonymousSession, verifySessionIdAndResolveUser } from "@/services/auth/session";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  // regCodeを受け取ってユーザー初期化処理を開始する
+});
+
