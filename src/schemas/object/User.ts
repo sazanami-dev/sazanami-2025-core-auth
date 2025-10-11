0bc88a639301874361f40ca9bd1efc88ab83a867
rca <nem@nem.x0.to>
2025-10-11 22:53:18 +0900

    wip: extend User schema

--- src/schemas/object/User.ts
+++ src/schemas/object/User.ts
@@ -4,3 +4,10 @@ export const UserSchema = z.object({
   id: z.nanoid().openapi({ description: 'ユーザーID', example: 'V1StGXR8_Z5jdHi6B-myT' }),
   displayName: z.string().min(1).max(100).nullable().openapi({ description: '表示名', example: '山田 太郎' }), // 名称未設定のユーザーも存在しうるため
 }).openapi('User');
+
+export const UserWithSessionSchema = UserSchema.extend({
+  hasPendingRedirect: z.boolean().openapi({ description: 'ユーザーに保留中のリダイレクトがあるかどうか', example: false }),
+}).openapi('UserWithSession');
+
+export type User = z.infer<typeof UserSchema>;
+export type UserWithSession = z.infer<typeof UserWithSessionSchema>;
