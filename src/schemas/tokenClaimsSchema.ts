11cb8ef45a53e58b51aaa8a20781379cc15519b1
rca <nem@nem.x0.to>
2025-10-11 13:53:41 +0900

    fix: remove uid claim(duplicated)

--- src/schemas/tokenClaimsSchema.ts
+++ src/schemas/tokenClaimsSchema.ts
@@ -11,7 +11,6 @@ export const TokenClaimsSchema = z.object({
 
   // Custom Claims
   jti: z.string().optional(), // JWT ID: Unique identifier for the token
-  uid: z.string(), // User ID: Custom claim for user identification
 }).openapi('TokenClaims');
 
 export type TokenClaims = z.infer<typeof TokenClaimsSchema>;
