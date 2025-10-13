3f03f8fd4ef9d7aaba527cf7c769b721f9a2ba7d
rca <nem@nem.x0.to>
2025-10-14 03:27:58 +0900

    fix: テスト時はDB接続テストをBypassするように(version関数がないっぽいので)

--- src/boot/test-connection.ts
+++ src/boot/test-connection.ts
@@ -1,5 +1,6 @@
 import prisma from "@/prisma";
 import Logger from "@/logger";
+import { EnvUtil, EnvKey } from "@/utils/env-util";
 
 export const testPsqlConnection = async () => {
   const dbLogger = new Logger('boot', 'psql');
@@ -7,6 +8,10 @@ export const testPsqlConnection = async () => {
   dbLogger.info('Testing PostgreSQL connection...');
 
   try {
+    if (EnvUtil.get(EnvKey.NODE_ENV) === 'test') {
+      dbLogger.info('Running in test mode, skipping actual database connection.', 'psql');
+      return;
+    }
     await prisma.$connect();
     const result = await prisma.$queryRaw`SELECT version()` as [{ version: string }];
     dbLogger.success("Success: " + result[0].version.split(" ").slice(0, 2).join(" "));
