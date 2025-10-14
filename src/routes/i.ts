7a0f80f4c839f23f90f3163ff9ee79010f25ba3b
rca <nem@nem.x0.to>
2025-10-15 03:39:52 +0900

    refactor: 共通化

--- src/routes/i.ts
+++ src/routes/i.ts
@@ -1,29 +1,34 @@
-import { Router } from "express";
+import { Request, Router } from "express";
 import { DoResponse } from "@/utils/do-resnpose";
 import { getUserWithSessionBySessionId } from "@/services/auth/user";
-import { UserWithSessionSchema } from "@/schemas/object/User";
+import { UserWithSession, UserWithSessionSchema } from "@/schemas/object/User";
+import { updateUserById } from "@/services/auth/user";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const cookies = req.cookies;
-  const sessionId = cookies ? (cookies.sessionId as string | undefined) : undefined;
-
-  if (!sessionId) {
-    return DoResponse.init(res).unauthorized().errorMessage('No session').send();
-  }
-
-  const userWithSession = await getUserWithSessionBySessionId(sessionId).catch(() => null);
-
-  if (!userWithSession) {
-    return DoResponse.init(res).unauthorized().errorMessage('Invalid session').send();
-  }
-
+  const userWithSession = await verifySessionAndGetUserHelper(req).catch(err => {
+    return DoResponse.init(res).unauthorized().errorMessage(err.message).send();
+  });
   return DoResponse.init(res).ok().validatedJson(userWithSession, UserWithSessionSchema).send();
 });
 
 router.put('/', async (req, res) => {
-  // ユーザー情報を書き換える
 });
+
+
+// Helper function to verify session
+async function verifySessionAndGetUserHelper(req: Request): Promise<UserWithSession> {
+  const cookies = req.cookies;
+  const sessionId = cookies ? (cookies.sessionId as string | undefined) : undefined;
+  if (!sessionId) {
+    throw new Error('No session');
+  }
+  const userWithSession = await getUserWithSessionBySessionId(sessionId).catch(() => null);
+  if (!userWithSession) {
+    throw new Error('Invalid session');
+  }
+  return userWithSession;
+}
 
 export default router;
