631f8e2ba0873c96255ad7798beabb18c238ea9e
rca <_@r0m.me>
2025-10-15 16:29:50 +0900

    improve: improve logics

--- src/routes/i.ts
+++ src/routes/i.ts
@@ -9,24 +9,29 @@ const router = Router();
 const logger = new Logger('routes', 'i');
 
 router.get('/', async (req, res) => {
-  const userWithSession = await verifySessionAndGetUserHelper(req).catch(err => {
-    return DoResponse.init(res).unauthorized().errorMessage(err.message).send();
-  });
+  const userWithSession = await verifySessionAndGetUserHelper(req);
+  if (!userWithSession) {
+    return DoResponse.init(res).unauthorized().errorMessage('Unauthorized').send();
+  }
+
   return DoResponse.init(res).ok().validatedJson(userWithSession, UserWithSessionSchema).send();
 });
 
 router.put('/', async (req, res) => {
-  const userWithSession = await verifySessionAndGetUserHelper(req).catch(err => {
-    return DoResponse.init(res).unauthorized().errorMessage(err.message).send();
-  });
+  const userWithSession = await verifySessionAndGetUserHelper(req);
+  if (!userWithSession) {
+    return DoResponse.init(res).unauthorized().errorMessage('Unauthorized').send();
+  }
 
   let updateData: Partial<User>;
+
   try {
     updateData = UserSchema.partial().parse(req.body);
   } catch (e) {
     logger.error('Failed to update user', e as string);
     return DoResponse.init(res).badRequest().errorMessage('Invalid request body').send();
   }
+
   const updatedUser = await updateUserById(userWithSession!.id, updateData).catch(err => {
     logger.error('Failed to update user', err);
     return DoResponse.init(res).internalServerError().errorMessage('Failed to update user').send();
@@ -36,15 +41,15 @@ router.put('/', async (req, res) => {
 });
 
 // Helper function to verify session
-async function verifySessionAndGetUserHelper(req: Request): Promise<UserWithSession> {
+async function verifySessionAndGetUserHelper(req: Request): Promise<UserWithSession | null> {
   const cookies = req.cookies;
   const sessionId = cookies ? (cookies.sessionId as string | undefined) : undefined;
   if (!sessionId) {
-    throw new Error('No session');
+    return null;
   }
   const userWithSession = await getUserWithSessionBySessionId(sessionId).catch(() => null);
   if (!userWithSession) {
-    throw new Error('Invalid session');
+    return null;
   }
   return userWithSession;
 }
