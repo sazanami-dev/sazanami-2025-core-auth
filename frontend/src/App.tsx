2da93ecd7e6716dc46d7fc8cc12437b207271bd4
rca <nem@nem.x0.to>
2025-11-02 17:36:23 +0900

    impl: fix routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import InitializePage from "./pages/init";
+import ReauthPage from "./pages/reauth";
 
 function App() {
   return (
     <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
+      <Route path="/init" element={<InitializePage />} />
+      <Route path="/reauth" element={<ReauthPage />} />
     </Routes>
   );
 }
