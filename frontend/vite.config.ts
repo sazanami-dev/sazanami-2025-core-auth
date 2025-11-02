bbf38a37858cfcee84edb73094d3b857aed0e2e8
rca <nem@nem.x0.to>
2025-11-02 16:10:07 +0900

    front: build: add base config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  
+  base: '/fe/',
 });
