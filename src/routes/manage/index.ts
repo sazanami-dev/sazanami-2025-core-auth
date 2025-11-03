c683ebcb3a609016d0f2f7d2d7a72b8ab227d64b
rca <nem@nem.x0.to>
2025-11-03 22:36:28 +0900

    impl: add static file provider

--- src/routes/manage/index.ts
+++ src/routes/manage/index.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import apiRouter from "./api/index.js";
 
 const router = Router();
 
 router.use("/api", apiRouter);
+
+// Provide ./public as static files
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+router.use(express.static(path.join(__dirname, "public")));
 
 export default router;
