bc9bf0904a7d4087e086adbad4bb7dd6bab31e24
rca <nem@nem.x0.to>
2025-11-03 22:21:41 +0900

    chore: cleanup

--- src/routes/manage/api/checkKey.ts
+++ src/routes/manage/api/checkKey.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import verifyManageKeyMiddleware from "@/middlewares/verify-manage-key";
 
 const router = Router();
-router.get("/", verifyManageKeyMiddleware, (req, res) => {
+router.get("/", verifyManageKeyMiddleware, (_req, res) => {
   res.json({ valid: true });
 });
