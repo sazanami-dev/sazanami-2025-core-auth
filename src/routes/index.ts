2d8b8ca46a51ff591304e5c871a8a0ca8e4c42b6
rca <nem@nem.x0.to>
2025-11-03 21:05:43 +0900

    impl: regist debug router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,25 @@
 import { Router } from "express";
+import { EnvKey, EnvUtil } from "@/utils/env-util";
 import iRoute from "./i";
 import authenticateRoute from "./authenticate";
 import initializeRoute from "./initialize";
 import wellKnownRoute from "./well-known";
 import verifyRoute from "./verify";
+import debugRoute from "./debug";
+import Logger from "@/logger";
 
 const router = Router();
+const logger = new Logger("route", "index");
 
 router.use('/i', iRoute);
 router.use('/authenticate', authenticateRoute);
 router.use('/initialize', initializeRoute);
 router.use('/.well-known', wellKnownRoute);
 router.use('/verify', verifyRoute);
+
+if (EnvUtil.get(EnvKey.NODE_ENV) !== 'production') {
+  router.use('/debug', debugRoute);
+  logger.warn("Debug routes are enabled.");
+}
 
 export default router;
