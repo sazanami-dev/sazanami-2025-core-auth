31bb576b1bfe01e0f4dd7da2db3ecd3569003c4f
rca <nem@nem.x0.to>
2025-11-03 23:46:36 +0900

    impl: regist task

--- src/crons/index.ts
+++ src/crons/index.ts
@@ -1,10 +1,12 @@
 import nodeCron from "node-cron";
 
 import sweepPendingRedirect from "./tasks/sweep-pending-redirect";
+import recordSystemState from "./tasks/record-system-state";
 
 async function startCrons() {
   // Sweep pending redirects every 2 minutes
   nodeCron.schedule("*/2 * * * *", sweepPendingRedirect);
+  nodeCron.schedule("*/5 * * * *", recordSystemState);
 }
 
 export default startCrons;
