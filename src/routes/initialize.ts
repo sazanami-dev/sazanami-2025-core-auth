80b0e39375dc5b77a647c29046315e0c6cc199ae
rca <_@r0m.me>
2025-11-03 12:50:31 +0900

    improve: エラーレスポンスの代わりにエラーページへのリダイレクトを行うように

--- src/routes/initialize.ts
+++ src/routes/initialize.ts
@@ -6,6 +6,7 @@ import { EnvKey, EnvUtil } from "@/utils/env-util";
 import { verifyRegCodeAndResolveUser } from "@/services/auth/regCode";
 import { PendingRedirect } from "@prisma/client";
 import { createPendingRedirect, getPendingRedirect } from "@/services/auth/pending-redirect";
+import { makeErrorPageUrlHelper } from "@/utils/make-error-page-url-helper";
 import Logger from "@/logger";
 
 const router = Router();
@@ -16,12 +17,14 @@ const SESSION_COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 2; // 2 days
 router.get('/', async (req, res) => {
   const reqCode = req.query.regCode as string | undefined;
   if (!reqCode) {
-    return DoResponse.init(res).badRequest().errorMessage('regCode query parameter is required').send();
+    const errorPageUrl = makeErrorPageUrlHelper('REQUIRED_PARAMETER_MISSING', '必須パラメーターが欠落しています。', 'regCode query parameter is required!');
+    return DoResponse.init(res).redirect(errorPageUrl.toString()).send();
   }
 
   const user = await verifyRegCodeAndResolveUser(reqCode).catch(() => null);
   if (!user) {
-    return DoResponse.init(res).badRequest().errorMessage('Invalid regCode').send();
+    const errorPageUrl = makeErrorPageUrlHelper('INVALID_REGCODE', '無効な登録コードです。', 'The provided regCode is invalid or has already been used.');
+    return DoResponse.init(res).redirect(errorPageUrl.toString()).send();
   }
 
   const { sessionId: oldSessionId } = req.cookies;
