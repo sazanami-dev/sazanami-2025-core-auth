9b51380377212d371dc0c844003eddace53c9797
rca <_@r0m.me>
2025-11-03 11:46:35 +0900

    impl: add /error route to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import InitializePage from "./pages/init";
 import ReauthPage from "./pages/reauth";
 import PortalPage from "./pages/portal";
+import ErrorPage from "./pages/error";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<PortalPage />} />
       <Route path="/init" element={<InitializePage />} />
       <Route path="/reauth" element={<ReauthPage />} />
+      <Route path="/error" element={<ErrorPage />} />
     </Routes>
   );
 }
