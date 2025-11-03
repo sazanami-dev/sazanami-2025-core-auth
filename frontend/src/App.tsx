b10684e7cb747ab10fe65a51418fd26b36959c0e
rca <_@r0m.me>
2025-11-03 11:38:04 +0900

    impl: add portal page placeholder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import InitializePage from "./pages/init";
 import ReauthPage from "./pages/reauth";
+import PortalPage from "./pages/portal";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<PortalPage />} />
       <Route path="/init" element={<InitializePage />} />
       <Route path="/reauth" element={<ReauthPage />} />
     </Routes>
