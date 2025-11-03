3c7295d19bf1a897bfb3d21db457be9ece4d86dc
rca <_@r0m.me>
2025-11-03 12:44:52 +0900

    impl: create makeErrorpageUrlHelper

---
+++ src/utils/make-error-page-url-helper.ts
@@ -0,0 +1,12 @@
+import { EnvKey, EnvUtil } from "./env-util";
+
+export function makeErrorPageUrlHelper(code: string, message?: string, detail?: string): URL {
+  const url = new URL(EnvUtil.get(EnvKey.ERROR_PAGE));
+  url.searchParams.append('code', code);
+  if (message) {
+    url.searchParams.append('message', message);
+  }
+  if (detail) {
+    url.searchParams.append('detail', detail);
+  }
+}
