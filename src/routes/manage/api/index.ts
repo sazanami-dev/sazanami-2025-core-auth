faa7b340d890a715338c90b75bb01bed64bdafb1
rca <nem@nem.x0.to>
2025-11-04 00:42:07 +0900

    impl: regist to index router

--- src/routes/manage/api/index.ts
+++ src/routes/manage/api/index.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import checkKeyRouter from "./checkKey";
 import eventRouter from "./event";
+import userRouter from "./user";
+import regCodeRouter from "./regCode";
+import sessionRouter from "./session";
+import pendingRedirectRouter from "./pending-redirect";
 
 const router = Router();
 
 router.use("/checkKey", checkKeyRouter);
 router.use("/event", eventRouter);
+router.use("/user", userRouter);
+router.use("/regCode", regCodeRouter);
+router.use("/session", sessionRouter);
+router.use("/pending-redirect", pendingRedirectRouter);
 
 export default router;
