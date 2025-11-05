435f5e2e0f257bdceab3f0e16bbf18b8b3180e02
rca <nem@nem.x0.to>
2025-11-05 21:08:56 +0900

    fix: remove import for /debug, following codes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import { EnvKey, EnvUtil } from "@/utils/env-util";
 import iRoute from "./i";
 import authenticateRoute from "./authenticate";
 import initializeRoute from "./initialize";
 import wellKnownRoute from "./well-known";
 import verifyRoute from "./verify";
-import debugRoute from "./debug";
 import manageRoute from "./manage";
-import Logger from "@/logger";
 
 const router = Router();
-const logger = new Logger("route", "index");
 
 router.use('/i', iRoute);
 router.use('/authenticate', authenticateRoute);
@@ -18,10 +14,5 @@ router.use('/initialize', initializeRoute);
 router.use('/.well-known', wellKnownRoute);
 router.use('/verify', verifyRoute);
 router.use('/manage', manageRoute);
-
-if (EnvUtil.get(EnvKey.NODE_ENV) !== 'production') {
-  router.use('/debug', debugRoute);
-  logger.warn("Debug routes are enabled.");
-}
 
 export default router;
