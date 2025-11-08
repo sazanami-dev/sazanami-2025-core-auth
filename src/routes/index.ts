8dccd8f92e061bb6ebed8172f7eee31a65460ef1
rca <nem@nem.x0.to>
2025-11-08 17:35:10 +0900

    impl: regist route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import initializeRoute from "./initialize";
 import wellKnownRoute from "./well-known";
 import verifyRoute from "./verify";
 import manageRoute from "./manage";
+import redirectRoute from "./redirect";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use('/initialize', initializeRoute);
 router.use('/.well-known', wellKnownRoute);
 router.use('/verify', verifyRoute);
 router.use('/manage', manageRoute);
+router.use('/redirect', redirectRoute);
 
 export default router;
