a94abc7b7efd7b04a339f3fb23ab74d0ae654533
rca <nem@nem.x0.to>
2025-11-08 13:15:07 +0900

    fix: add isInitialized, create ApiUserSchema

--- src/schemas/object/User.ts
+++ src/schemas/object/User.ts
@@ -3,7 +3,12 @@ import z from '@/zod';
 export const UserSchema = z.object({
   id: z.string().openapi({ description: 'ユーザーID', example: 'V1StGXR8_Z5jdHi6B-myT' }),
   displayName: z.string().min(1).max(100).nullable().openapi({ description: '表示名', example: '山田 太郎' }), // 名称未設定のユーザーも存在しうるため
+  isInitialized: z.boolean().openapi({ description: 'ユーザーが初期化されているかどうか', example: true }),
 }).openapi('User');
+
+export const ApiUserSchema = UserSchema.omit({
+  isInitialized: true,
+}).openapi('ApiUser');
 
 export const UserWithSessionSchema = UserSchema.extend({
   hasPendingRedirect: z.boolean().openapi({ description: 'ユーザーに保留中のリダイレクトがあるかどうか', example: false }),
@@ -11,3 +16,4 @@ export const UserWithSessionSchema = UserSchema.extend({
 
 export type User = z.infer<typeof UserSchema>;
 export type UserWithSession = z.infer<typeof UserWithSessionSchema>;
+export type ApiUser = z.infer<typeof ApiUserSchema>;
