f77c09ed74380a28e76d438e87c39c38f3f31e3c
rca <nem@nem.x0.to>
2025-11-11 18:11:35 +0900

    impl: Reauthページの説明を追加

--- frontend/src/pages/reauth.tsx
+++ frontend/src/pages/reauth.tsx
@@ -1,4 +1,5 @@
 import { Accordion, AccordionItem } from "@heroui/accordion";
+import { Button } from "@heroui/button";
 import { PiSealWarningDuotone } from "react-icons/pi";
 
 export default function ReauthPage() {
@@ -8,21 +9,30 @@ export default function ReauthPage() {
         <PiSealWarningDuotone className="text-6xl text-yellow-500" />
         <h1 className="text-3xl my-4">認証が必要です</h1>
       </div>
-      <div className="w-full max-w-md flex flex-col justify-center">
+      <div className="w-full max-w-xl flex flex-col justify-center">
         <Accordion>
           <AccordionItem
             key="with-card"
             aria-label="カードを使用して認証する"
             title="カードを使用して認証する"
           >
-            ここに説明 1
+            <p><span className="font-semibold">さざなみ開発が出展するブース</span>や<span className="font-semibold">一部のチェックポイントポスター</span>に設置されている"ログインカード"を1枚取り、カードに貼り付けられているQRコードをスキャンしてください。</p>
+            <p>スキャン後、自動で処理を継続します</p>
+            <p className="text-gray-600 mt-3">※ 必ず<span className="font-semibold">このブラウザ</span>でQRコードを処理してください。</p>
           </AccordionItem>
           <AccordionItem
             key="without-card"
             aria-label="カードを使用せずに認証する"
             title="カードを使用せずに認証する"
           >
-            ここに説明 2
+            <p>下のボタンから登録を行ってください。</p>
+            <div className="flex justify-center mt-4">
+              <Button
+                className="w-md mt-5"
+                color="primary"
+                variant="shadow"
+              >登録ページ</Button>
+            </div>
           </AccordionItem>
         </Accordion>
       </div>
