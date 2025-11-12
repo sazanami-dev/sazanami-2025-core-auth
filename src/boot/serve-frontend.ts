7d858873bef2ed9ae8e115a7bd10d2423dd167f4
rca <nem@nem.x0.to>
2025-11-13 00:16:18 +0900

    chore: cleanup

--- src/boot/serve-frontend.ts
+++ src/boot/serve-frontend.ts
@@ -7,24 +7,20 @@ import { fileURLToPath } from 'url';
 const logger = new Logger('boot', 'frontend');
 
 export const serveFrontend = (app: Express) => {
-
-  // ../public/fe
-
   if (EnvUtil.get(EnvKey.NODE_ENV) === 'development') {
     logger.info('Skipping frontend serving in development mode.');
     logger.info('HINT: In development mode, the frontend is served by the Vite dev server.');
     return;
   }
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const frontendPath = path.join(__dirname, '../../../public/fe');
   logger.info(`Serving frontend static files from: ${frontendPath}`);
   app.use('/fe', express.static(frontendPath));
 
-
   app.get('/fe/{*any}', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
   });
-  
+
   logger.info('Frontend serving middleware has been set up.');
 }
