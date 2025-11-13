8feae8f9ed7fe4a186e9942da1b0965c71fd425c
rca <nem@nem.x0.to>
2025-11-14 02:32:01 +0900

    impl: impl portal page WIP

--- frontend/src/pages/portal.tsx
+++ frontend/src/pages/portal.tsx
@@ -1,11 +1,72 @@
-import FloatingBubbles from "@/components/floating-bubble";
+import SiteLinkCard from "@/components/portal/site-link-card"
 
 export default function PortalPage() {
+  const siteLinks: Parameters<typeof SiteLinkCard>[0][] = [
+    {
+      siteName: "さざなみスタンプラリー",
+      siteUrl: "srally.nem.one",
+      siteIconUrl: "https://github.com/r-ca.png",
+      siteDescription: "すたんぷらりー",
+      siteTags: [
+        { label: "さざなみ開発", color: "primary", variant: "bordered" },
+        { label: "要認証", color: "warning", variant: "dot" },
+      ],
+      linkUrl: "https://srally.nem.one",
+    },
+    {
+      siteName: "さざなみスタンプラリー",
+      siteUrl: "srally.nem.one",
+      siteIconUrl: "https://github.com/r-ca.png",
+      siteDescription: "すたんぷらりー",
+      siteTags: [
+        { label: "さざなみ開発", color: "primary", variant: "bordered" },
+        { label: "要認証", color: "warning", variant: "dot" },
+      ],
+      linkUrl: "https://srally.nem.one",
+    },
+    {
+      siteName: "さざなみスタンプラリー",
+      siteUrl: "srally.nem.one",
+      siteIconUrl: "https://github.com/r-ca.png",
+      siteDescription: "すたんぷらりー",
+      siteTags: [
+        { label: "さざなみ開発", color: "primary", variant: "bordered" },
+        { label: "要認証", color: "warning", variant: "dot" },
+      ],
+      linkUrl: "https://srally.nem.one",
+    },
+    {
+      siteName: "さざなみスタンプラリー",
+      siteUrl: "srally.nem.one",
+      siteIconUrl: "https://github.com/r-ca.png",
+      siteDescription: "すたんぷらりー",
+      siteTags: [
+        { label: "さざなみ開発", color: "primary", variant: "bordered" },
+        { label: "要認証", color: "warning", variant: "dot" },
+      ],
+      linkUrl: "https://srally.nem.one",
+    },
+    {
+      siteName: "さざなみスタンプラリー",
+      siteUrl: "srally.nem.one",
+      siteIconUrl: "https://github.com/r-ca.png",
+      siteDescription: "すたんぷらりー",
+      siteTags: [
+        { label: "さざなみ開発", color: "primary", variant: "bordered" },
+        { label: "要認証", color: "warning", variant: "dot" },
+      ],
+      linkUrl: "https://srally.nem.one",
+    }
+  ]
+
   return <>
-    <div className="flex flex-col justify-center items-center min-h-screen p-4">
-      <FloatingBubbles />
-      <p className="text-3xl"
-      >Portal Page</p>
+    <div className="container mx-auto p-4">
+      <h1 className="mb-6 text-2xl font-bold">ポータル</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center w-full">
+        {siteLinks.map((siteLink, index) => (
+          <SiteLinkCard key={index} {...siteLink} />
+        ))}
+      </div>
     </div>
   </>
 }
