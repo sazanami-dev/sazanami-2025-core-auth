e033f4c2244b422dd7a3148b11622bef864cb10d
rca <nem@nem.x0.to>
2025-11-14 03:01:34 +0900

    impl: impl settings modal wip

--- frontend/src/pages/portal.tsx
+++ frontend/src/pages/portal.tsx
@@ -1,4 +1,10 @@
 import SiteLinkCard from "@/components/portal/site-link-card"
+import { Button } from "@heroui/button"
+import { PiGearDuotone } from "react-icons/pi"
+import SettingsModal, { Settings } from "@/components/portal/settings-modal"
+import useApi from "@/hooks/useApi"
+import { useEffect, useState } from "react"
+import { IResponseSchema } from "@/types/api/i"
 
 export default function PortalPage() {
   const siteLinks: Parameters<typeof SiteLinkCard>[0][] = [
@@ -59,14 +65,59 @@ export default function PortalPage() {
     }
   ]
 
+  const [settings, setSettings] = useState<Settings>({ displayName: "" });
+  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+
+  // Modal
+  useEffect(() => {
+    const api = useApi();
+    api.get("/i").then((response) => {
+      const data = IResponseSchema.parse(response.data);
+      setSettings({ displayName: data.displayName || "" });
+    }).catch((error) => {
+      console.error("Failed to fetch user info:", error);
+    });
+  }, []);
+
+  async function updateSettings(newSettings: Settings) {
+    const api = useApi();
+    try {
+      await api.put("/i", {
+        displayName: newSettings.displayName,
+      });
+      setSettings(newSettings);
+    } catch (error) {
+      console.error("Failed to update settings:", error);
+    }
+  }
+
+
   return <>
     <div className="container mx-auto p-4">
-      <h1 className="mb-6 text-2xl font-bold">ポータル</h1>
+      <header className="flex flex-row justify-between items-center mb-4">
+        <h1 className="text-2xl">ポータル</h1>
+        <Button isIconOnly variant="bordered" size="md">
+          <PiGearDuotone className="text-xl" />
+        </Button>
+      </header>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center w-full">
         {siteLinks.map((siteLink, index) => (
           <SiteLinkCard key={index} {...siteLink} />
         ))}
       </div>
     </div>
+
+    {/* <SettingsModal */}
+    {/*   isOpen={isSettingsModalOpen} */}
+    {/*   currentSettings={settings} */}
+    {/*   onSave={(newSettings) => setSettings(newSettings)} */}
+    {/*   onClose={() => setIsSettingsModalOpen(false)} */}
+    {/* /> */}
+    <SettingsModal
+      isOpen={isSettingsModalOpen}
+      currentSettings={settings}
+      onSave={updateSettings}
+      onClose={() => setIsSettingsModalOpen(false)}
+    />
   </>
 }
