4dcaddcc6f1c89b56a0fc668fe48da8ce70fc0a1
rca <nem@nem.x0.to>
2025-11-14 04:08:17 +0900

    Revert "wip: wip"
    This reverts commit 0c250b3eea4f41bdb280913d718d916e8f314b89.

--- src/routes/manage/api/index.ts
+++ src/routes/manage/api/index.ts
@@ -6,7 +6,6 @@ import userRouter from "./user";
 import regCodeRouter from "./regCode";
 import sessionRouter from "./session";
 import pendingRedirectRouter from "./pending-redirect";
-import schemaRouter from "./schema";
 
 const router = Router();
 
@@ -17,6 +16,5 @@ router.use("/user", userRouter);
 router.use("/regCode", regCodeRouter);
 router.use("/session", sessionRouter);
 router.use("/pending-redirect", pendingRedirectRouter);
-router.use("/schema", schemaRouter);
 
 export default router;
